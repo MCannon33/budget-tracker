448c00510d5d233892754b814bc09cf1f759638b
MCannon33 <marialcannon@gmail.com>
2021-03-16 03:19:20 -0400

    trying to fix deploy

--- Develop/server.js
+++ Develop/server.js
@@ -22,7 +22,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useUnifiedTopology: true,
 });
 
-// routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
